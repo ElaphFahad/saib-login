af4d2ee270054e46d30dd5ba057594df1eb932c9
ElaphFahad <92299382+ElaphFahad@users.noreply.github.com>
2021-12-09 13:21:55 +0300

    ReactNAtive Login

---
+++ app/config/colors.jsx
@@ -0,0 +1,10 @@
+export default {
+    primary: '#edb415',
+    secondary: '#262522',
+    white: '#fff',
+    black: '#000',
+    medium: '#6e6969',
+    light: '#f8f4f4',
+    dark: '#0c0c0c',
+    danger: '#ff5252'
+}
